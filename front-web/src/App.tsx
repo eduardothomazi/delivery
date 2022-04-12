d9117f997df2fdd72cf886d97a3ce024e5e18bfd
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-04-12 13:10:24 -0300

    Creating order post frontend

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,16 +1,17 @@
 import './App.css';
 import Footer from './Footer';
-
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './Navbar';
 import RoutesSite from './RouterPages';
 
 function App() {
   return (
     <div className="App">
-      
-        <Navbar/>
-        <RoutesSite/>
-        <Footer />
+      <Navbar />
+      <RoutesSite />
+      <Footer />
+      <ToastContainer />
     </div>
   );
 }
