9964878e42c7a508b3a83894b67b9d580d44ede8
Eduardo Thomazi <eduardothomazi91@gmail.com>
2022-04-12 14:07:04 -0300

    Setting db do prod

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -9,7 +9,7 @@ export function fetchProducts(){
 }
 
 export function fetchLocalMapBox( local: string){
-    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+    return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}/orders.json?access_token=${mapboxToken}`)
 }
 
 export function saveOrder(payload: OrderPayload){
